ac07b52e5a7ca134d1a4253f723769755ca2654f
Ryan Webber <rdmwebber@hotmail.com>
2024-09-05 15:44:04 -0400

    moved more functionality to truck class so main was more readable

--- truck.py
+++ truck.py
@@ -1,13 +1,15 @@
 from datetime import timedelta
 class Truck:
 
-  def __init__(self,packages,departureTime):
+  def __init__(self,packages,departureTime,currentLocation,gpsMap):
     self.packages = packages
+    self.gpsMap = gpsMap
     self.depatureTime = departureTime
     self.speed = 18
     self.time = departureTime
     self.capacity = 16
     self.mileage = 0
+    self.currentLocation = currentLocation
 
   def updateTime(self,distance):
     self.time += timedelta(distance/self.speed)
@@ -16,3 +18,35 @@ class Truck:
     self.mileage+=miles
 
 
+  #Check distance to next address
+  def checkDistance(self,locationOne,locationTwo):
+    curAddress= self.gpsMap.getValue(locationOne)
+    nextAddress= self.gpsMap.getValue(locationTwo)
+
+  #distance reference isn't bidirectional so you have to check both x(y) and y(x)
+    if curAddress.getDistance(nextAddress.index) == "":
+     return nextAddress.getDistance(curAddress.index)
+    else: 
+      return curAddress.getDistance(nextAddress.index)
+   
+   # find which package in the truck that hasn't been delivered is closest  
+  def nextClosest(self):
+    minDistance = float('inf')
+    closestPackage = self.packages[0]
+
+    for pkg in self.packages:
+
+      distance = self.checkDistance(self.currentLocation,pkg.address)
+
+      if pkg.status != "Delivered" and distance < minDistance:
+        minDistance = distance
+        closestPackage = pkg
+    
+    return closestPackage
+
+
+  def loadTruck(self,packageHashMap, packageIDs)
+    for id in packageIDs:
+      
+    
+ 
